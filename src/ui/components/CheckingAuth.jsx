5d7008eb813cc704c15ef7def224256561445818
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-08 17:26:54 -0400

    se crea el spinner con el componente CheckingAuth

--- src/ui/components/CheckingAuth.jsx
+++ src/ui/components/CheckingAuth.jsx
@@ -6,7 +6,7 @@ export const CheckingAuth = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-900 p-1 mx-auto flex-col" >
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-green-600"></div>
-      <span className="mt-3 text-green-600" >Cargando</span>
+      <span className="mt-3 text-green-600 animate-pulse" >Cargando</span>
     </div>
   )
 }
