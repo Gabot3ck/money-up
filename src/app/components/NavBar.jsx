869ed9f4c34c0bd80d027433b8691907996a1753
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-12 16:46:56 -0400

    se valida la vista del btn burguer del NavBar

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -2,14 +2,16 @@ import { HamburguerMenu } from "./HamburguerMenu";
 import Logo from '../../assets/img/Logo Navbar.png';
 
 
-export const Navbar = () => {
+export const Navbar = ( { showBurger = true }) => {
   return (<>
     <div className="w-full h-[60px] p-2
                     border-b border-gray-300
                     flex justify-between items-center" >
       <img  className='h-[40px] ' src={ Logo } alt="Logotipo Money App" />
 
-      <HamburguerMenu />
+      {
+        showBurger && <HamburguerMenu />
+      }
     </div>
   </>
   )
