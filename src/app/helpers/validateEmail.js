c50d12b91cd7ff8f530912a014823060790db414
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-15 15:42:29 -0400

    se crea el validateEmail

---
+++ src/app/helpers/validateEmail.js
@@ -0,0 +1,17 @@
+export const validateEmail = (e, setValited, setError) => {
+  const valueInput = e.target.value;
+  const regName = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if( valueInput.trim() !== "" ){
+          if( !regName.test(e.target.value.trim())){
+              setValited(false);
+              setError("El email ingresado no es válido");
+          } else {
+              setValited(true);
+              setError('');
+          }
+      } else {
+          setValited(false);
+          setError("Debe ingresar un email");
+      }
+}
