c390de7aeb0e3d5ebd1f331093adb9d57b3bd779
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-15 13:15:21 -0400

    se crean las funciones de registro con google en login y register

--- src/app/components/forms/FormLogin.jsx
+++ src/app/components/forms/FormLogin.jsx
@@ -1,12 +1,48 @@
-import { ButtonPrimary } from "../../../ui/components/buttons/ButtonPrimary"
+import { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FaFacebook, FaGoogle } from "react-icons/fa"
+import { checkingAuthentication, startGoogleSignIn } from "../../../store/auth"
+import { useForm } from "../../../hooks/useForm"
+import { OrOfButtons } from "../../../ui/components/OrOfButtons"
+import { ButtonPrimary, ButtonSecondary } from "../../../ui/components/buttons"
+
 
 
 export const FormLogin = () => {
 
+  const { status } = useSelector( state => state.auth );
 
-  return (
-    <form className="w-full 
-                    flex px-4 flex-col justify-center items-center gap-8" 
+
+  const dispatch = useDispatch();
+  const { email, password, onInputChange } = useForm({
+    email: 'fernando@google.com',
+    password: '123456',
+  });
+
+  const isAuthenticating = useMemo( () => status === 'checking', [status]);
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({ email, password })
+    dispatch( checkingAuthentication() );
+  }
+
+  const onGoogleSignIn = () => {
+    dispatch( startGoogleSignIn() );
+  }
+
+  const onFacebookSignIn = () => {
+    console.log('onFacebookSignIn');
+  }
+
+
+  return (<>
+        <form 
+      className="w-full 
+                flex px-4 flex-col justify-center items-center gap-8"
+      onSubmit={ onSubmit }
     >
       <input
         className="w-full h-10 rounded py-2 px-4 
@@ -15,6 +51,9 @@ export const FormLogin = () => {
                   bg-primary-300 placeholder-gray-500"
         type="email" 
         placeholder="Email"
+        name="email"
+        value={ email }
+        onChange={ onInputChange }
       />
       <input
         className="w-full h-10 rounded py-2 px-4 
@@ -23,8 +62,39 @@ export const FormLogin = () => {
                   bg-primary-300 placeholder-gray-500"
         type="password" 
         placeholder="Contraseña"
+        name="password"
+        value={ password }
+        onChange={ onInputChange }
       />
       <ButtonPrimary title="Ingresar" type='submit' />
     </form>
+
+    <div className="w-full px-4 my-8
+                    flex flex-col gap-8 items-center justify-center " 
+    >
+      <OrOfButtons />
+
+      <ButtonSecondary
+        icon={ <FaGoogle /> }
+        title={'Ingresar con Google'}
+        onClick= { onGoogleSignIn }
+        disabled={ isAuthenticating }
+      />
+
+      <ButtonSecondary
+        icon={ <FaFacebook /> }
+        title={'Ingresar con Facebook'}
+        onClick= { onFacebookSignIn }
+        disabled={ isAuthenticating }
+      />
+
+      <div className="w-full flex items-center justify-center gap-3 mt-6">
+        <p className="text-sm" >¿No está registrado?</p>
+        <p className="text-sm text-primary-500 font-medium" >Regístrese aquí</p>
+      </div>
+
+    </div>
+  </>
+
   )
 }
--- src/app/components/forms/FormRegister.jsx
+++ src/app/components/forms/FormRegister.jsx
@@ -1,14 +1,32 @@
+import { useForm } from "../../../hooks/useForm";
 import { ButtonPrimary } from "../../../ui/components/buttons"
+
+
+const formData = {
+  email: 'contacto@ebgchile.cl',
+  password: '123456',
+  confirmPassword: '4657',
+  name: 'Contacto EBG',
+}
 
 
 export const FormRegister = () => {
 
+  const { name, email, password, confirmPassword, onInputChange, formState } = useForm(formData);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formState)
+  }
 
 
 
   return (
-    <form className="w-full 
-                    flex px-4 flex-col justify-center items-center gap-8" 
+    <form
+      onSubmit={ onSubmit }
+      className="w-full 
+                flex px-4 flex-col justify-center items-center gap-8" 
     >
       <input
         className="w-full h-10 rounded py-2 px-4 
@@ -17,6 +35,9 @@ export const FormRegister = () => {
                   bg-primary-300 placeholder-gray-500"
         type="text" 
         placeholder="Nombre"
+        name="name"
+        value={ name }
+        onChange={ onInputChange }
       />
 
       <input
@@ -26,6 +47,9 @@ export const FormRegister = () => {
                   bg-primary-300 placeholder-gray-500"
         type="email" 
         placeholder="Email"
+        name="email"
+        value={ email }
+        onChange={ onInputChange }
       />
 
       <input
@@ -35,6 +59,9 @@ export const FormRegister = () => {
                   bg-primary-300 placeholder-gray-500"
         type="password" 
         placeholder="Contraseña"
+        name="password"
+        value={ password }
+        onChange={ onInputChange }
       />
 
       <input
@@ -44,6 +71,9 @@ export const FormRegister = () => {
                   bg-primary-300 placeholder-gray-500"
         type="password" 
         placeholder="Confirmar Contraseña"
+        name="confirmPassword"
+        value={ confirmPassword }
+        onChange={ onInputChange }
       />
 
       <ButtonPrimary title="Registrarme" type='submit' />
