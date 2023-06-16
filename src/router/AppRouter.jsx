e5d7edc3239e46a910973f330706d0b014c21a2f
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-16 16:56:35 -0400

    se modifica la ruta en appRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,28 @@
-import {  Route, Routes } from "react-router-dom"
-import { AppRoutes } from "../app/routes/AppRoutes"
-import { AuthRoutes } from "../auth/routes/AuthRoutes"
+import {  Navigate, Route, Routes } from "react-router-dom";
+import { useCheckAuth } from "../hooks/useCheckAuth";
+import { AppRoutes } from "../app/routes/AppRoutes";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { CheckingAuth } from "../ui";
+
 
 export const AppRouter = () => {
 
+  const status  = useCheckAuth();
+
+  if( status === 'checking' ) return <CheckingAuth /> 
 
 
   return (
     <Routes>
-      <Route path="/auth/*" element={ <AuthRoutes /> } />
-      <Route path='/*' element={ <AppRoutes /> } />
 
+      {
+        (status === 'authenticated')
+        ? <Route path='/*' element={ <AppRoutes /> } />
+        : <Route path="/auth/*" element={ <AuthRoutes /> } />
+      }
+
+      <Route path="/*" element={ <Navigate to='/auth/home' /> } />
+      
     </Routes>
   )
 }
