0c1e648dd951bb20958a1e35c65f86f4535005d4
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-16 16:54:57 -0400

    se modifican los componentes de ui

--- src/ui/components/buttons/ButtonPrimary.jsx
+++ src/ui/components/buttons/ButtonPrimary.jsx
@@ -1,11 +1,12 @@
 
 
-export const ButtonPrimary = ( { title = 'Registrar' } ) => {
+export const ButtonPrimary = ( { title = 'Registrar', isChecking= false } ) => {
 
 
   return (
     
-    <button 
+    <button
+      disabled= { isChecking }
       className="w-full sm:max-w-[160px] h-[40px]
                 text-sm sm:text-base text-dark-500 font-semibold
                 bg-primary-500  rounded-full 
