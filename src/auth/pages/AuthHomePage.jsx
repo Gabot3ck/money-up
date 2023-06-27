9a995934c084698561c5c24daef3d28a355d7a47
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-27 11:46:40 -0400

    se plica responsive a AuthHomePage

--- src/auth/pages/AuthHomePage.jsx
+++ src/auth/pages/AuthHomePage.jsx
@@ -1,25 +1,25 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { ButtonPrimary } from "../../ui/components/buttons/ButtonPrimary";
 import Logo from '../../assets/img/Logo Mobile vertical.png';
 
 
 export const AuthHomePage = () => {
   return (
-    <div className="w-screen h-screen px-8 py-8
+    <div className="w-screen min-h-screen px-8 py-8 m-0
                     flex flex-col items-center justify-center
                     bg-light-500" 
     >
-      <div className="w-[320px  h-[60%] mb-3
+      <div className="w-[320px]  h-[60%] mb-3
                       flex items-center justify-center">
         <img src={ Logo } alt="Logotipo Money Up" />
       </div>
 
-      <h1 className="font-semibold text-lg text-center mb-2">
-        Organicemos juntos <br /> tus finanzas
+      <h1 className="w-[165px] sm:w-[266px]
+      font-semibold text-lg text-center mb-2">
+        Organicemos juntos tus finanzas
       </h1>
 
-      <p className="text-sm text-center font-normal my-7"
-      >
+      <p className="text-sm text-center font-normal my-7">
         Ya has dado el primer paso, ¡Enhorabuena! <br /> Déjanos guiárte en este proceso
       </p>
 
@@ -31,7 +31,6 @@ export const AuthHomePage = () => {
                 transition-colors duration-300 ease-out"
         to="/register"
       >
-      
         <ButtonPrimary />
       </Link>
       
