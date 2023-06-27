11f75bd15896b0a6def03f76670a3ee771b622d5
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-27 12:18:57 -0400

    se modifican el responsive de los btn

--- src/ui/components/buttons/ButtonPrimary.jsx
+++ src/ui/components/buttons/ButtonPrimary.jsx
@@ -7,7 +7,7 @@ export const ButtonPrimary = ( { title = 'Registrar', isChecking= false } ) => {
     
     <button
       disabled= { isChecking }
-      className="w-full sm:max-w-[160px] h-[40px]
+      className="w-full  h-[40px]
                 text-sm sm:text-base text-dark-500 font-semibold
                 bg-primary-500  rounded-full 
                 hover:bg-primary-400 
--- src/ui/components/buttons/ButtonSecondary.jsx
+++ src/ui/components/buttons/ButtonSecondary.jsx
@@ -5,7 +5,7 @@ export const ButtonSecondary = ( { icon, title, onClick, disabled } ) => {
     <button
       disabled={ disabled }
       onClick={ onClick }
-      className="w-full sm:max-w-[320px] h-[40px] 
+      className="w-full h-[40px] 
                 flex items-center justify-center 
                 text-primary-500 text-sm sm:text-base
                 border  border-primary-500 rounded-full
@@ -13,7 +13,7 @@ export const ButtonSecondary = ( { icon, title, onClick, disabled } ) => {
                 hover:text-primary-400  hover:border-primary-400
                 transition-colors duration-300 ease-out" 
       >
-      <span className="w-[40%] pr-5  sm:w-[20%] flex justify-end " >
+      <span className="w-[40%] pr-5  sm:w-[40%] flex justify-end " >
         { icon }
       </span>
 
