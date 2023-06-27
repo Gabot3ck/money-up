f85caadfb090f258ae972f3e0b51a5cf093931df
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-27 15:41:27 -0400

    se aplica responsive a RegisterSalary

--- src/app/views/RegisterSalary.jsx
+++ src/app/views/RegisterSalary.jsx
@@ -5,11 +5,12 @@ export const RegisterSalary = () => {
   
   
   return (<>
-    <div className="w-full px-4 py-2 h-full
-                    flex flex-col justify-start items-center " >
+    <div className="px-4 py-2 
+                    w-full h-full md:w-3/4 max-w-[500px]
+                    flex flex-col justify-start items-center" 
+    >
       <div className=" w-full px-4 mt-4 mb-20 ">
         <h3 className="w-full font-medium text-xl text-start mb-2" >Registrar sueldo</h3>
-
         <p className="text-base" >¿Cuál es su sueldo mensual actual?</p>
       </div>
 
