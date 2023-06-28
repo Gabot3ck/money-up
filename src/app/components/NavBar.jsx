15b5bd002f6e593f2620f25c8154632277799312
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-28 08:57:06 -0400

    se mejore la UX del Nav

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -4,7 +4,7 @@ import Logo from '../../assets/img/Logo Navbar.png';
 
 export const Navbar = ( { showBurger = true }) => {
   return (<>
-    <div className="w-full max-w-screen-lg h-[60px] p-2
+    <div className="w-full max-w-screen-lg h-[60px] py-2 px-4
                     border-b border-gray-300
                     flex justify-between items-center" >
       <img  className='h-[40px] ' src={ Logo } alt="Logotipo Money App" />
