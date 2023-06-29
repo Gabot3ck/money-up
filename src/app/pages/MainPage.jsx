15b4f288e002e398d24e4fb13f82b118572a7ec9
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-06-29 15:41:32 -0400

    se crea el MainPage

---
+++ src/app/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import { FooterMain } from "../../ui"
+import { AppLayaout } from "../layaout/AppLayaout"
+
+
+export const MainPage = () => {
+  return (
+    <AppLayaout >
+      <h1>Main Page</h1>
+      <FooterMain />
+    </AppLayaout>
+  )
+}
